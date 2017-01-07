fc11a9e67a57ad7b72fa0de60e89931da120032b
Vignesh <Vignesh@192.168.43.135>
2017-01-07 18:01:09 +0530

    Selenium Demo

---
+++ selenium/src/seleniumDemo/SeleniumDemo.java
@@ -0,0 +1,15 @@
+package seleniumDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:/Vignesh/Selenium/chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
