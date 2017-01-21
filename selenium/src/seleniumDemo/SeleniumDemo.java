d6000010479c7812638c789878c27d9337e1bb0f
Vignesh <Vignesh@192.168.43.135>
2017-01-21 15:09:55 +0530

    Final

--- selenium/src/seleniumDemo/SeleniumDemo.java
+++ selenium/src/seleniumDemo/SeleniumDemo.java
@@ -3,7 +3,6 @@ package seleniumDemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
